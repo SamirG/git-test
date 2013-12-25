a2fdab38ed44a97045c606fe25861dc33c65b10d
Samir Ghodasara <samir@apalya.myplex.tv>
2013-12-25 19:58:53 +0530

    sysout

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -5,7 +5,8 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Main.main() 1");
+		System.out.println("Main.main() 2");
 
 	}
 
